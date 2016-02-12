1a4b5349d63714228dfa65a2c455c6429787030e
Aniruddha Sinha <Aniruddha.Sinha@bitwiseglobal.com>
2016-02-12 23:55:34 +0530

    Few Tests Added

---
+++ src/main/java/com/aniruddha/parkinglot/Car.java
@@ -0,0 +1,12 @@
+package com.aniruddha.parkinglot;
+
+/**
+ * Created by Aniruddha Sinha on 12-02-2016.
+ */
+public class Car {
+    private String carNumber;
+
+    public Car(String carNumber) {
+        this.carNumber = carNumber;
+    }
+}
---
+++ src/main/java/com/aniruddha/parkinglot/NullLotCreatedException.java
@@ -0,0 +1,7 @@
+package com.aniruddha.parkinglot;
+
+/**
+ * Created by Aniruddha Sinha on 12-02-2016.
+ */
+public class NullLotCreatedException extends RuntimeException {
+}
